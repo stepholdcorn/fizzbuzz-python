397bdcdfb22fb6a19d4e298320879f5126cfef51
steph <stephieoldcorn@gmail.com>
2015-03-23 10:34:23 +0000

    Adds a play method

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,17 +1,18 @@
 def is_divisible_by_three(number):
-  if number % 3 == 0:
-    return 'Fizz'
-  else:
-    return number
+  return number % 3 == 0
 
 def is_divisible_by_five(number):
-  if number % 5 == 0:
-    return 'Buzz'
-  else:
-    return number
+  return number % 5 == 0
 
 def is_divisible_by_three_and_five(number):
-  if number % 3 == 0 & number % 5 == 0:
+  return number % 3 == 0 and number % 5 == 0
+
+def play(number):
+  if is_divisible_by_three_and_five(number):
     return 'Fizzbuzz'
+  elif is_divisible_by_three(number):
+    return 'Fizz'
+  elif is_divisible_by_five(number):
+    return 'Buzz'
   else:
     return number
--- tests/fizzbuzz_tests.py
+++ tests/fizzbuzz_tests.py
@@ -2,13 +2,19 @@ from nose.tools import *
 from fizzbuzz import fizzbuzz
 
 def test_is_divisible_by_three():
-  assert_equal(fizzbuzz.is_divisible_by_three(3), 'Fizz')
-  assert_equal(fizzbuzz.is_divisible_by_three(4), 4)
+  assert_equal(fizzbuzz.is_divisible_by_three(3), True)
+  assert_equal(fizzbuzz.is_divisible_by_three(4), False)
 
 def test_is_divisible_by_five():
-  assert_equal(fizzbuzz.is_divisible_by_five(5), 'Buzz')
-  assert_equal(fizzbuzz.is_divisible_by_five(4), 4)
+  assert_equal(fizzbuzz.is_divisible_by_five(5), True)
+  assert_equal(fizzbuzz.is_divisible_by_five(4), False)
 
 def test_is_divisible_by_three_and_five():
-  assert_equal(fizzbuzz.is_divisible_by_three_and_five(15), 'Fizzbuzz')
-  assert_equal(fizzbuzz.is_divisible_by_three_and_five(4), 4)
+  assert_equal(fizzbuzz.is_divisible_by_three_and_five(15), True)
+  assert_equal(fizzbuzz.is_divisible_by_three_and_five(4), False)
+
+def test_play():
+  assert_equal(fizzbuzz.play(3), 'Fizz')
+  assert_equal(fizzbuzz.play(5), 'Buzz')
+  assert_equal(fizzbuzz.play(15), 'Fizzbuzz')
+  assert_equal(fizzbuzz.play(4), 4)
